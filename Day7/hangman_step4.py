3d9b737354142175bbc2a09bac6a1b9f25a9c765
Sid44444 <46089927+Sid44444@users.noreply.github.com>
2025-04-07 17:18:05 +0100

    Add files via upload
    Lives variable added. If guess not in word lives reduced by one.  ASCII art displayed depending on live variable value.

---
+++ Day7/hangman_step4.py
@@ -0,0 +1,116 @@
+import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
++---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+ +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+''', '''
++---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+ +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+word_list = ["aardvark", "baboon", "camel"]
+
+#TODO-1: Create a variable called 'lives' to keep track of the number of lives left. Set lives to equal 6.
+lives = 6
+
+chosen_word = random.choice(word_list)  # randomly chooses a word from the word_list and assigns it to a variable called chosen_word
+print(chosen_word)
+
+# Create an empty string called "placeholder".
+# for each letter in chosen_word, add a _ to placeholder
+# same number of blanks as the word.
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
+
+# Use a while loop to let the user guess again
+
+game_over = False
+correct_letters = []  # correct guesses placed in list that is used to print the guesses to date
+
+while not game_over:
+    guess = input("Guess a letter: ").lower()  # ask user to guess a letter and assign their answer to a variable called guess. Make lowercase.
+    #print (guess)
+
+    # Create a display that put the guess letter in the right place.
+
+    display = ""
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print(display)
+
+    #TODO-2 If guess is not a letter in the chosen_word. Then reduce lives by 1.
+    # If lives goes down to 0 then the game should stop and it should print "You lose"
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            game_over = True
+            print ("You lose.")
+
+
+    if "_" not in display: #no more letters to guess
+        game_over = True
+        print("You win")
+
+# TODO-3: print the ASCII art from 'stages'
+# that corresponds to the current number of lives the user has remaining.
+    print(stages[lives])
