265b8a8a9dac6b1e575a5e303109e5580cb1fe60
Sid44444 <46089927+Sid44444@users.noreply.github.com>
2025-04-09 16:12:23 +0100

    Add files via upload
    functions and key pair arguments added

---
+++ functions_and_inputs.py
@@ -0,0 +1,15 @@
+#age = int(input("What is your current age? "))
+
+#def life_in_weeks(age):
+    #weeks_alive= age * 52
+    #return weeks_alive
+
+#ninety_years_in_weeks = 4680
+
+#print (f"You have {ninety_years_in_weeks - life_in_weeks(age)} weeks left" )
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+greet_with( location="Nowhere", name ="Jack")#using key pairs
