fd1a05afb3a84c9b5ad3c3f4c06b7fc97d83a7fa
Sid44444 <46089927+Sid44444@users.noreply.github.com>
2025-04-14 11:44:19 +0100

    Add files via upload
    code changed to use a for loop but possible output can more than two digits

--- Day8/love_calculator.py
+++ Day8/love_calculator.py
@@ -8,18 +8,17 @@ def calculate_love_score(name1, name2):#no need to keep name separate so combine
     combined_names = name1 + name2
     lower_names =combined_names.lower()
     #print (lower_names)
+    #for letter in combined_names.lower():
+    first_digit, second_digit = 0, 0
+    #for loop used below but if score above 9 the score is printed
+    # as such eg truetrue and lovelove prints out 1010
+    for digit in [lower_names]:
 
-    t = lower_names.count("t")
-    r = lower_names.count("r")
-    u = lower_names.count("u")
-    e = lower_names.count("e")
-    first_digit = t + r + u + e #may be a better way of doing this as the code repeats
+        for letter in ["t", "r", "u", "e"]:
+            first_digit += lower_names.count(letter)
 
-    l = lower_names.count("l")
-    o = lower_names.count("o")
-    v = lower_names.count("v")
-    e = lower_names.count("e")
-    second_digit = l + o + v + e
+        for letter in ["l", "o", "v", "e"]:
+            second_digit += lower_names.count(letter)
 
     score = int(str(first_digit) + str(second_digit))
     return score
