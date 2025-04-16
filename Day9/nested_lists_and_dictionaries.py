825435e1d15a52a2ac05f1c038cb54ac51ef47e2
Sid44444 <46089927+Sid44444@users.noreply.github.com>
2025-04-16 18:31:04 +0100

    Add files via upload
    accessing lists in dictionaries example

---
+++ Day9/nested_lists_and_dictionaries.py
@@ -0,0 +1,13 @@
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin"
+}
+
+#Nested list in dictionary
+
+travel_log = {
+    "France": ["Paris", "Lille", "Dijion"],
+    "Germany": ["Stuttgart", "Berlin"],
+
+}
+print (travel_log["France"][1]) #prints Lille using the key value "France"
