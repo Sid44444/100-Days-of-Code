15d0c225b4690f3bf2bd56bd372badb295ad2bf0
Sid44444 <46089927+Sid44444@users.noreply.github.com>
2025-05-06 16:39:38 +0100

    Add files via upload
    Initial commit with dictionary and operation functions

---
+++ Day10/calculator.py
@@ -0,0 +1,24 @@
+"""A simple calculator app"""
+input(int("What is the first number?: " ))
+print("+\n-\n*\n/\n")
+print ("Pick an operation: ")
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
