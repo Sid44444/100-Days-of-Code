9b203bf16c3495a516523c3f2d83f969ead070a3
Sid44444 <46089927+Sid44444@users.noreply.github.com>
2025-05-08 12:01:41 +0100

    Add files via upload
    number and operation inputs requested and used in the operation which is printed out with answer using the dictionary of functions and parameters provided as inputs.

--- Day10/calculator.py
+++ Day10/calculator.py
@@ -1,8 +1,8 @@
 """A simple calculator app"""
-#input(int("What is the first number?: " ))
-#print("+\n-\n*\n/\n")
-#print ("Pick an operation: ")
 
+
+
+#operation functions
 def add(n1, n2):
     return n1 + n2
 
@@ -15,11 +15,21 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
+#operation dictionary
 operations = {
     "+": add,
     "-": subtract,
     "*": multiply,
     "/": divide,
 }
+#def calculation(f_num, operation, s_num):
 
-print (operations["*"](4, 8))
+#print (operations["*"](4, 8))
+
+f_num = float(input("What is the first number?: "))
+for symbol in operations:
+    print(symbol)
+operation_symbol = input("Pick an operation: ")
+s_num = float(input("What is the next number?: "))
+#prints calculation and answer using operation_symbol to access the dictionary of functions with parameters
+print(f"{f_num} {operation_symbol} {s_num} = {operations[operation_symbol](f_num, s_num)}")
