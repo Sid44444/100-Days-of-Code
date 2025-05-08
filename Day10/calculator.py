997d6e9f52e9665eecc322443fc962a508156284
Sid44444 <46089927+Sid44444@users.noreply.github.com>
2025-05-08 17:41:11 +0100

    Add files via upload
    function using recursion to start the function again to perform operations

--- Day10/calculator.py
+++ Day10/calculator.py
@@ -1,5 +1,5 @@
 """A simple calculator app"""
-
+from socket import EAGAIN
 
 
 #operation functions
@@ -26,10 +26,28 @@ operations = {
 
 #print (operations["*"](4, 8))
 
-f_num = float(input("What is the first number?: "))
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation: ")
-s_num = float(input("What is the next number?: "))
+def calculator():
+    should_accumulate = True
+    f_num = float(input("What is the first number?: "))
+
+    while should_accumulate:
+
+
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        s_num = float(input("What is the next number?: "))
 #prints calculation and answer using operation_symbol to access the dictionary of functions with parameters
-print(f"{f_num} {operation_symbol} {s_num} = {operations[operation_symbol](f_num, s_num)}")
+        answer = operations[operation_symbol](f_num, s_num)
+        print(f"{f_num} {operation_symbol} {s_num} = {answer}")
+        use_result = input(f"Do you want to continue working with the previous result {answer}? ('y' for yes or 'n' for a new calculation: ")
+        if use_result == "y":
+            f_num = answer
+        else:
+            should_accumulate = False
+            print("\n" * 20)
+            calculator()#calling a function within a function to begin again (recursion)
+
+
+calculator()#call function to start the program
+
