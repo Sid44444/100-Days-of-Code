84cad4d2563ecc20088d9888bcd6ba6b93a7e97e
Sid44444 <46089927+Sid44444@users.noreply.github.com>
2025-05-23 16:17:46 +0100

    Add files via upload
    Debugging exercise using print to locate the bug line of code. Comments solve the issue.

---
+++ Day 13/print_used_to_debug_code.py
@@ -0,0 +1,10 @@
+#words_per_page = 0
+pages = int(input("Number of pages: "))
+print (pages)
+words_per_page == (int(input("Number of words per page: ")))
+print (words_per_page)
+total_words = (pages * words_per_page)
+print (total_words)
+
+#The double = sign on line 4  if returning False instead of the number
+#A single = sign should be used to correct the code.
