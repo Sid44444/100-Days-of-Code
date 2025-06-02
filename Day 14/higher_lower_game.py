a8df7cf9e62d13ff39437df18e89486072027039
Sid44444 <46089927+Sid44444@users.noreply.github.com>
2025-06-02 21:44:39 +0100

    Add files via upload
    Initial commit with basic code

---
+++ Day 14/higher_lower_game.py
@@ -0,0 +1,27 @@
+import random
+from art import logo
+from art import vs
+from game_data import data
+print (logo)
+print(vs)
+num= random.randint (0, 49)
+print (num +1)
+print (data[num])
+
+#TODO print higher lower art
+#import art
+#print art when game starts
+#TODO Compare A: name, a occupation, from country
+#select randon number in list and print out name, occupation anf country using the key
+#TODO VS art
+#import art
+#print art
+#TODO- Generate random numbers for a and b
+#TODO Comparison line
+#TODO Against B: name, a occupation, from country
+#TODO Print who has more followers? Type A or B:
+#TODO compare the follower_count in the dictuionaries in the list.
+#TODO If input = the highest follower_count win . else lose.
+#TODO The previous A now becomes B and a new comparison happens
+#TODO "Sorry, that's wrong. Final score: 3"
+#TODO "You're right! Current score: 1,
