b2fa08399996bf27dbd1a755a25bc73d9b9f3453
Sid44444 <46089927+Sid44444@users.noreply.github.com>
2025-06-03 12:02:39 +0100

    Add files via upload
    game almost complete. Two TODO's to complete.

--- Day 14/higher_lower_game.py
+++ Day 14/higher_lower_game.py
@@ -1,27 +1,45 @@
 import random
+from socket import EAGAIN
+
 from art import logo
 from art import vs
 from game_data import data
-print (logo)
-print(vs)
-num= random.randint (0, 49)
-print (num +1)
-print (data[num])
 
-#TODO print higher lower art
-#import art
-#print art when game starts
-#TODO Compare A: name, a occupation, from country
-#select randon number in list and print out name, occupation anf country using the key
-#TODO VS art
-#import art
-#print art
-#TODO- Generate random numbers for a and b
-#TODO Comparison line
-#TODO Against B: name, a occupation, from country
-#TODO Print who has more followers? Type A or B:
-#TODO compare the follower_count in the dictuionaries in the list.
-#TODO If input = the highest follower_count win . else lose.
+score = int(0)
+def game(score):
+    print(logo)
+    num= random.randint (0, 49)
+    num2= random.randint (0, 49)
+    print (num +1)
+    A_name= (data[num]["name"])
+    A_occupation = (data[num]["description"])
+    A_country = (data[num]["country"])
+    A_follower_count = (data[num]["follower_count"])
+    B_name= (data[num2]["name"])
+    B_occupation = (data[num2]["description"])
+    B_country = (data[num2]["country"])
+    B_follower_count = (data[num2]["follower_count"])
+    print(f"Compare A: {A_name}, a {A_occupation}, from {A_country}.")
+    print(vs)
+    print(f"Against B: {B_name}, a {B_occupation}, from {B_country}.")
+    answer=input("Who has more followers? Type 'A' or 'B': ").upper()
+    if A_follower_count > B_follower_count and answer == 'A':
+        score += 1
+        print (f"You're right! Current score: {score}")
+        game(score)
+
+    elif A_follower_count < B_follower_count and answer == 'B':
+        score  += 1
+        print (f"You're right! Current score: {score}")
+        game(score)
+    else:
+        print (f"Sorry, that's wrong. Final score: {score}")
+        play_again =input ("Would you like to play again? Type 'Y' or 'N'" ).upper
+        if play_again == 'Y':
+            game(score)
+
+
+game(score)
 #TODO The previous A now becomes B and a new comparison happens
-#TODO "Sorry, that's wrong. Final score: 3"
-#TODO "You're right! Current score: 1,
+#TODO Get play again to work
+
