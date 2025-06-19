122f42f4d52d462c907214f8a0be506ad99578c1
Sid44444 <46089927+Sid44444@users.noreply.github.com>
2025-06-19 16:43:12 +0100

    Add files via upload
    Different colours added using a list and random.choice

--- Day18/shapes.py
+++ Day18/shapes.py
@@ -1,9 +1,12 @@
 from turtle import Turtle, Screen
+import random
 
 tim = Turtle()
 screen = Screen()
 tim.shape("turtle")
 tim.color("red")
+
+colours = ["BlueViolet", "brown1", "cyan1", "DarkGoldenrod", "DarkOliveGreen1", "coral2", "DeepPink2"]
 
 def draw_shape(num_sides):
     angle = 360 / num_sides
@@ -12,6 +15,7 @@ def draw_shape(num_sides):
         tim.right(angle)
 
 for shape_side_n in range(3, 11):
+    tim.color(random.choice(colours))
     draw_shape(shape_side_n)
 
 screen.exitonclick()
